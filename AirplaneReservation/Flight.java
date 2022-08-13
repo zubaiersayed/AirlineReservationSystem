fa9c6ac71ce058251f14ac84ce197dc17e5e46e1
Zubaier <zubaier10@hotmail.com>
2022-08-13 12:58:56 +0200

    changes to Flight.java

--- AirplaneReservation/Flight.java
+++ AirplaneReservation/Flight.java
@@ -1,13 +1,35 @@
 package AirplaneReservation;
 
-import java.time.Clock;
+import java.util.Random;
 
 public class Flight {
     private String flightID;
-    private Clock clock;
+    private double clock;
     private String destination;
     private Seat seatNumber;
 
+    public Flight(String destination){
+        this.flightID = "";
+        this.clock = new Random().nextDouble();
+        this.destination = destination;
+        this.seatNumber = null;
+    }
 
+    public void setFlightID(String flightID) {
+        this.flightID = flightID;
+    }
+
+    public void setClock(double clock) {
+        this.clock = clock;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public void setSeatNumber(Seat seatNumber) {
+        this.seatNumber = seatNumber;
+    }
 
 }
+
